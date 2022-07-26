08abdf48c723165651eff7be261cbc5e2653b87e
Starlight258 <audwl03071@gmail.com>
2022-07-26 23:20:37 +0900

    Feat:URL 추가

--- savemarket/marketapp/views.py
+++ savemarket/marketapp/views.py
@@ -3,3 +3,38 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+def login(request):
+    return render(request, 'login.html')
+def beauty(request):
+    return render(request, 'beauty.html')
+def food(request):
+    return render(request, 'food.html')
+def stationery(request):
+    return render(request, 'stationery.html')
+def book(request):
+    return render(request, 'book.html')
+def life(request):
+    return render(request, 'life.html')
+def interior(request):
+    return render(request, 'interior.html')
+def pet(request):
+    return render(request, 'pet.html')
+def sport(request):
+    return render(request, 'sport.html')
+def etc(request):
+    return render(request, 'etc.html')
+def create(request):
+    return render(request, 'create.html')
+def wishlist(request):
+    return render(request, 'wishlist.html')
+def joinlist(request):
+    return render(request, 'joinlist.html')
+def chat(request):
+    return render(request, 'chat.html')
+def alarm(request):
+    return render(request, 'alarm.html')
+def account(request):
+    return render(request, 'account.html')
+def digital(request):
+    return render(request, 'digital.html')
+
--- savemarket/savemarket/urls.py
+++ savemarket/savemarket/urls.py
@@ -19,5 +19,23 @@ from marketapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-]
+    path('', views.home, name='home'), #홈화면
+    path('login/', views.login, name='login'), #로그인화면
+    path('beauty/', views.beauty, name='beauty'), #뷰티
+    path('food/', views.food, name='food'), #음식
+    path('stationery/', views.stationery, name='stationery'), #문구
+    path('book/', views.book, name='book'), #책
+    path('life/', views.life, name='life'), #생활
+    path('interior/', views.interior, name='interior'), #인테리어
+    path('pet/', views.pet, name='pet'), #펫
+    path('digital/', views.digital, name='digital'), #디지털
+    path('sport/', views.sport, name='sport'), #스포츠
+    path('etc/', views.etc, name='etc'), #etc
+    path('create/', views.create, name='create'), #공구 생성창
+    path('wishlist/', views.wishlist, name='wishlist'), #공구 생성창
+    path('joinlist/', views.joinlist, name='joinlist'), #공구 목록
+    path('chat/', views.chat, name='chat'), #채팅 목록
+    path('alarm/', views.alarm, name='alarm'), #알람 목록
+    path('account/', views.account, name='account'), #회원가입
+
+]     
